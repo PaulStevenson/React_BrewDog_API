4d56ab534f80d3e0153c24bc222668cd847ca741
Paul <rpmstevenson@gmail.com>
2018-07-17 15:02:57 +0100

    CSS added

--- src/components/BeerDetail.js
+++ src/components/BeerDetail.js
@@ -3,8 +3,8 @@ import React from 'react';
 const BeerDetail = (props) => {
     if (!props.beer) return null;
     return (
-        <div>
-            <h3>{props.beer.name}</h3>
+        <div id="details">
+            <h2>{props.beer.name}</h2>
             <p>{props.beer.description}</p>
             <p>ABV: {props.beer.abv}%</p>
             <img src={props.beer.image_url}/>
